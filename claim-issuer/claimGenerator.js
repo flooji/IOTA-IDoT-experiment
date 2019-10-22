ff3b094149db0c3fc6f7dc28e7a1244676ca399d
flooji <florella240@gmail.com>
2019-10-22 14:05:18 +0200

    change claim content

--- claim-issuer/claimGenerator.js
+++ claim-issuer/claimGenerator.js
@@ -1,10 +1,10 @@
 
-//This script produces a signed Identity-claim in form of a JSON-document and stores it on the raspberry pi
+//This script produces a signed Identity-claim in form of a JSON-document and stores it locally
 
 //uncommment these variables if you would like to execute the script itself (node make-ID-claim.js):
 const pubKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCTOdA8wN524+wb5gqaU5uiliT1S5LoNqDIA2SI5P7VgGsYaH6zByB5jA7+muYCFUWHnWupU4DtEB6D59XgGRLfstxuyOIb7lwW1stsHaQW1UmZ5d04OlwQW2bMHvm1CwEEaOkVij+d6hsMhPTuFnbu1C3KQlGbCTEe5OClvN8DPQIDAQAB"
-const channelID = "FIORYRCHDI9INGL9NSGU9ACALOPDDVVLERCMXLAUFHIQPVGKDIXBIKBFCWZPPFKXOKCGMWHEYBWWHDFJXCSKUUDLAB"
-const issuerName = "https://strassenverkehrsaemter.ch/"
+const signingAddress = "WWOTSUHSMXGELOPMDAHFSYXUGEHNIMGVUOSUHISVVUZYPQAQMSOXCWTIEKKYBCNVCBNBS9EWE9IVLDNAW"
+const issuerName = "https://publicRoadsAdministration.com/"
 const owner = "Transport GmbH"
 const model = "Raspberry Pi 3B+"
 const validUntil = "01/01/2025"
@@ -12,14 +12,14 @@ const validUntil = "01/01/2025"
 const uuidv4 = require('uuid/v4')
 const fs = require('fs')
 
-generateClaim = function (devicePubKey,root,issuer,deviceOwner,deviceModel,expirationDate) {
+generateClaim = function (devicePubKey,issuerAdress,issuer,deviceOwner,deviceModel,expirationDate) {
     try {
         const UUID = uuidv4();
     
         jsonClaim = {
             "subject": UUID,
             "devicePubKey": devicePubKey,
-            "root": root,
+            "issuerAddress": issuerAdress,
             "issuer": issuer,
             "data": {
                 "deviceOwner": deviceOwner,
@@ -35,12 +35,11 @@ generateClaim = function (devicePubKey,root,issuer,deviceOwner,deviceModel,expir
             console.log(`File claim_${UUID}.json created successfully.`)
         })
     
-    } catch(error) {
+    } catch(err) {
         
-        console.log('Error: ', error)
+        console.log('Error: ', err)
     }
 }
 
 //uncomment the function if you would like to execute the script itself:
-
-generateClaim(pubKey,channelID,issuerName,owner,model,validUntil)
+generateClaim(pubKey,signingAddress,issuerName,owner,model,validUntil)
